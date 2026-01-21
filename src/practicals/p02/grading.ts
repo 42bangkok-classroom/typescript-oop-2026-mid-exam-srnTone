8647943dc095356e8e30bb16459081e8ebca03cf
Saran Kongdam <68011813@kmitl.ac.th>
2026-01-21 14:32:40 +0700

    1

--- src/practicals/p02/grading.ts
+++ src/practicals/p02/grading.ts
@@ -1,1 +1,23 @@
 // Write your code below
+const input = process.argv[2];
+const number = Number(input);
+
+if (Number.isNaN(input)){
+    console.log("Invalid input");
+    process.exit(1)
+}   else if (number < 0 || number > 100){
+    console.log("Invalid input");
+    process.exit(1)
+}
+
+if (number >= 80){
+    console.log("Grade is A");
+}   else if (number >= 70){
+    console.log("Grade is B");
+}   else if (number >= 60){
+    console.log("Grade is C");
+}   else if (number >= 50){
+    console.log("Grade is D");
+}   else {
+    console.log("Grade is F");
+}
