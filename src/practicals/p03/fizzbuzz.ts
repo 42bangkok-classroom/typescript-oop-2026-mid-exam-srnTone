46c5bf2d41c7a5dbe8e9a553c8e5751e21a1591e
Saran Kongdam <68011813@kmitl.ac.th>
2026-01-21 16:10:04 +0700

    5

--- src/practicals/p03/fizzbuzz.ts
+++ src/practicals/p03/fizzbuzz.ts
@@ -1,1 +1,25 @@
 // Write your code below
+const input = process.argv[2];
+const number = Number(input);
+
+if (Number.isNaN(input)){
+    console.log("Invalid input");
+    process.exit()
+}   else if (number <= 0 ){
+    console.log("Invalid input");
+    process.exit()
+}
+
+for (let i = 1; i <= number; i++){
+    if (i%3==0&&i%5==0){
+    console.log("FizzBuzz");
+}   else if (i%5==0){
+    console.log("Buzz");
+}   else if (i%3==0){
+    console.log("Fizz");
+}
+    else {
+    console.log(i);
+}
+}
+
